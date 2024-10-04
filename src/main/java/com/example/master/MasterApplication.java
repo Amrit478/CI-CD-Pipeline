b29fb90d9f01308c2276003d6afed3d404f7eb24
Amrit478 <Amritraj4786@gmail.com>
2024-10-04 17:22:37 -0400

    five commit

--- src/main/java/com/example/master/MasterApplication.java
+++ src/main/java/com/example/master/MasterApplication.java
@@ -17,12 +17,5 @@ public class MasterApplication {
 	@GetMapping
 	public String hello1() {
 		return "Hello World!";
-	
+	}	
 }
-// echo "# CI-CD-Pipeline" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M master
-// git remote add origin https://github.com/Amrit478/CI-CD-Pipeline.git
-// git push -u origin master
