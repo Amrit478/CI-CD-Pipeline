a5d5ce6785a9beb58a0dcdb0bb00a4ece3c44580
Amrit478 <Amritraj4786@gmail.com>
2024-10-04 16:41:42 -0400

    second commit

--- src/main/java/com/example/master/MasterApplication.java
+++ src/main/java/com/example/master/MasterApplication.java
@@ -2,6 +2,7 @@ package com.example.master;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MasterApplication {
@@ -9,5 +10,16 @@ public class MasterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MasterApplication.class, args);
 	}
+	@GetMapping
+	public String hello() {
+		return "Hello World!";
+	}
 
 }
+// echo "# CI-CD-Pipeline" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Amrit478/CI-CD-Pipeline.git
+// git push -u origin main
